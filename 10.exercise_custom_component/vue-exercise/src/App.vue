4241ff777f7399df1a9a26aee01e702a1585e760
aysf <ansufw@gmail.com>
2023-09-25 13:19:23 +0200

    add properties to exercise chapter

---
+++ 10.exercise_custom_component/vue-exercise/src/App.vue
@@ -0,0 +1,69 @@
+<script>
+import BenderStatistics from './components/BenderStatistics.vue'
+
+export default {
+  components: {
+    BenderStatistics,
+  },
+  data: () => ({
+    characters: [
+      {
+        name: 'Aang',
+        element: ['Air', 'Water', 'Earth', 'Fire']
+      },
+      {
+        name: 'Zuko',
+        element: ['Fire']
+      },
+      {
+        name: 'Katara',
+        element: ['Water']
+      },
+      {
+        name: 'Toph',
+        element: ['Earth']
+      }
+    ],
+    favoriteList: [],
+    newCharacter: {
+      name: '',
+      element: []
+    }
+  }),
+  methods: {
+    addNewCharacter() {
+      this.characters.push(this.newCharacter)
+      this.newCharacter = { name: '' }
+    },
+    favoriteCharacters(char) {
+      this.favoriteList.push(char)
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>Characters</h1>
+
+  <!-- statistic -->
+  <BenderStatistics v-bind:charactersParam="characters" />
+
+  <h2>show characters in the paragraf</h2>
+  <p>
+    <span v-for="(char, idx) in characters">
+      {{ char.name }}{{ idx === characters.length - 1 ? '' : ', ' }}
+    </span>
+  </p>
+
+  <h2>show favorite characters</h2>
+  <p v-if="favoriteList.length > 0">
+    <span v-for="(char, idx) in favoriteList">
+      {{ char.name }}{{ idx === favoriteList.length - 1 ? '' : ', ' }}
+    </span>
+  </p>
+  <p v-else>no favorite Characters</p>
+
+  <h2>add new character</h2>
+  <label for="new-characters"></label>
+  <input type="text" v-model="newCharacter.name" @keyup.enter="addNewCharacter" />
+</template>
---
+++ 10.exercise_custom_component/vue-exercise/src/components/BenderStatistics.vue
@@ -0,0 +1,43 @@
+<script>
+export default{
+    props: ["charactersParam"],
+    computed: {
+    earthBenderList() {
+      return this.characters.filter((char) => char.element.indexOf('Earth') > -1)
+    },
+    benderStatistics() {
+      const elements = ['Air', 'Earth', 'Fire', 'Water']
+      const statistics = {
+        Air: 0,
+        Earth: 0,
+        Fire: 0,
+        Water: 0
+      }
+
+      this.charactersParam.forEach((char) => {
+        elements.forEach((element) => {
+          if (char.element.indexOf(element) > -1) {
+            statistics[element] += 1
+          }
+        })
+      })
+
+      return statistics
+    }
+  }
+}
+</script>
+
+<template>
+<h2>Statistics</h2>
+  <ul></ul>
+  {{ benderStatistics }}
+
+  <h2>show characters in the list</h2>
+  <ul>
+    <li v-for="char in charactersParam">
+      <p>{{ char.name }}</p>
+      <button @click="favoriteCharacters(char)">⭐ Favorite</button>
+    </li>
+  </ul>
+</template>
