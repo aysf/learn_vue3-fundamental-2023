dd017ded00e0c0d4830f4677d1b11f04a5132587
aysf <ansufw@gmail.com>
2023-09-28 12:35:48 +0200

    add: pokedex

---
+++ 15.fetch_data/vue-fetch/src/App.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data: () => ({
+    pokedex: [1,2,3]
+  }),
+  methods: {
+    async fetchPokemon() {
+      this.pokedex = await fetch("https://pokeapi.co/api/v2/pokemon?limit=155").then(response => response.json())
+    },
+  }
+}
+</script>
+
+<template>
+ <h1>Pokemon</h1>
+
+<pre>{{pokedex}}</pre>
+<button @click="fetchPokemon">fetch</button>
+
+</template>
+
+<style>
+
+</style>
