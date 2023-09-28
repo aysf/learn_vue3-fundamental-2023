a9a6770f6ff527d2c6a59b54668683034e063c67
aysf <ansufw@gmail.com>
2023-09-28 12:42:00 +0200

    add: created att to init fetchPokemon()

--- 15.fetch_data/vue-fetch/src/App.vue
+++ 15.fetch_data/vue-fetch/src/App.vue
@@ -1,24 +1,33 @@
 <script>
 export default {
+  beforeCreate() {
+    console.log('beforeCreate')
+    console.log(this.pokedex)
+  },
   data: () => ({
-    pokedex: [1,2,3]
+    pokedex: [1, 2, 3]
   }),
   methods: {
     async fetchPokemon() {
-      this.pokedex = await fetch("https://pokeapi.co/api/v2/pokemon?limit=155").then(response => response.json())
-    },
+      this.pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=155').then((response) =>
+        response.json()
+      )
+    }
+  },
+
+  created() {
+    console.log('created')
+    console.log(this.pokedex)
+    this.fetchPokemon()
   }
 }
 </script>
 
 <template>
- <h1>Pokemon</h1>
+  <h1>Pokemon</h1>
 
-<pre>{{pokedex}}</pre>
-<button @click="fetchPokemon">fetch</button>
-
+  <pre>{{ pokedex }}</pre>
+  <!-- <button @click="fetchPokemon">fetch</button> -->
 </template>
 
-<style>
-
-</style>
+<style></style>
